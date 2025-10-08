1c889e6976595e814e142ebc55929728c34d4d52
Dinesh Budhathoki <physmarika@gmail.com>
2025-10-08 18:28:50 +0800

    navbar is done

---
+++ DineshNote/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-gradient-to-r from-gray-900 via-gray-800 to-black shadow-md">
+      <div className="container mx-auto flex justify-between items-center h-16 px-6">
+        <div className="flex items-center gap-3">
+          <Link
+            to="/"
+            className="text-xl font-bold text-gray-300 tracking-wide hover:text-yellow-400 transition"
+          >
+            DineshNote
+          </Link>
+        </div>
+
+        {/* Navigation */}
+        <ul className="hidden md:flex space-x-5 text-sm text-gray-300 font-medium">
+          <li>
+            <a href="/#about" className="hover:text-yellow-400 cursor-pointer">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="/#skills" className="hover:text-yellow-400 cursor-pointer">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a
+              href="/#education"
+              className="hover:text-yellow-400 cursor-pointer"
+            >
+              Education
+            </a>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              className="hover:text-yellow-400 cursor-pointer"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/blogs" className="hover:text-yellow-400 cursor-pointer">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <a
+              href="/#contact"
+              className="hover:text-yellow-400 cursor-pointer"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        {/* Mobile Menu (optional for future) */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
