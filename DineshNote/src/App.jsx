77311ba337c5e08c755737b4c1e48319a2856080
Dinesh Budhathoki <physmarika@gmail.com>
2025-10-09 11:27:44 +0800

    footer is added

--- DineshNote/src/App.jsx
+++ DineshNote/src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Footer />
     </>
   );
 }
